395385fcf20fe0b49a8b187fe4f57893e2bac7e8
Matt Safaii <matt@mgency.com>
2021-05-20 11:17:12 -0700

    fix: add netlify tag to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,76 @@ export default function ContactPage() {
                 subheading="Contact Us"
                 heading="Want to place an order or inquire about a custom cake? Fill out the form below:"
             />
-            <Form />
+            <form netlify
+                display="grid"
+                col="1|repeat(2, auto)"
+                gap="4"
+                mt="4"
+                w="full|auto"
+            >
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="name">Name</label>
+                    </VisuallyHidden>
+                    <input
+                        variant="input"
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                    />
+                </div>
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="email">Email</label>
+                    </VisuallyHidden>
+                    <input
+                        variant="input"
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                    />
+                </div>
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="phone">Phone</label>
+                    </VisuallyHidden>
+                    <input
+                        variant="input"
+                        type="tel"
+                        id="phone"
+                        name="phone"
+                        placeholder="Phone"
+                    />
+                </div>
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="subject">Subject</label>
+                    </VisuallyHidden>
+                    <select variant="select" id="subject" name="subject">
+                        <option value="">Subject</option>
+                        <option value="one">Online Order</option>
+                        <option value="two">Custom Cake</option>
+                        <option value="two">Other</option>
+                    </select>
+                </div>
+                <div colStart="span 2">
+                    <VisuallyHidden>
+                        <label htmlFor="message">Message</label>
+                    </VisuallyHidden>
+                    <textarea
+                        variant="textarea"
+                        placeholder="Message"
+                        id="message"
+                        name="message"
+                        rows="5"
+                    />
+                </div>
+                <button type="submit" variant="button.primary" colStart="span 2">
+                    Send message
+                </button>
+            </form>
         </Layout>
     )
 }
