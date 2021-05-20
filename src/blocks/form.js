7b1324921ade7073282fed77442d9d0915ae1506
Matt Safaii <matt@mgency.com>
2021-05-20 15:37:41 -0700

    fix: add getform instead of netlify form

--- src/blocks/form.js
+++ src/blocks/form.js
@@ -1,89 +1,106 @@
-import * as React from "react"
+import React, { useState } from "react"
+import axios from "axios";
 import { VisuallyHidden } from "reflexjs"
+const MyForm = () => {
 
-export default function Block({
-    subheading,
-    heading,
-    text,
-    buttons,
-    ...props
-}) {
+    const [serverState, setServerState] = useState({
+        submitting: false,
+        status: null
+    });
+    const handleServerResponse = (ok, msg, form) => {
+        setServerState({
+            submitting: false,
+            status: { ok, msg }
+        });
+        if (ok) {
+            form.reset();
+        }
+    };
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        setServerState({ submitting: true });
+        axios({
+            method: "post",
+            url: "https://getform.io/f/81ed7aa2-aa5e-4dd2-9372-ff88ec830c04",
+            data: new FormData(form)
+        })
+            .then(r => {
+                handleServerResponse(true, "Thanks!", form);
+            })
+            .catch(r => {
+                handleServerResponse(false, r.response.data.error, form);
+            });
+    };
+
     return (
-        <section py="6|8|12" {...props}>
-            <div variant="container" width="100vw|65vw" my="12|0">
-                <div display="grid" col="1" gap="8|12|16" alignItems="center">
-                    <form netlify
-                        display="grid"
-                        col="1|repeat(2, auto)"
-                        gap="4"
-                        mt="4"
-                        w="full|auto"
-                    >
-                        <div>
-                            <VisuallyHidden>
-                                <label htmlFor="name">Name</label>
-                            </VisuallyHidden>
-                            <input
-                                variant="input"
-                                type="text"
-                                id="name"
-                                name="name"
-                                placeholder="Name"
-                            />
-                        </div>
-                        <div>
-                            <VisuallyHidden>
-                                <label htmlFor="email">Email</label>
-                            </VisuallyHidden>
-                            <input
-                                variant="input"
-                                type="email"
-                                id="email"
-                                name="email"
-                                placeholder="Email"
-                            />
-                        </div>
-                        <div>
-                            <VisuallyHidden>
-                                <label htmlFor="phone">Phone</label>
-                            </VisuallyHidden>
-                            <input
-                                variant="input"
-                                type="tel"
-                                id="phone"
-                                name="phone"
-                                placeholder="Phone"
-                            />
-                        </div>
-                        <div>
-                            <VisuallyHidden>
-                                <label htmlFor="subject">Subject</label>
-                            </VisuallyHidden>
-                            <select variant="select" id="subject" name="subject">
-                                <option value="">Subject</option>
-                                <option value="one">Online Order</option>
-                                <option value="two">Custom Cake</option>
-                                <option value="two">Other</option>
-                            </select>
-                        </div>
-                        <div colStart="span 2">
-                            <VisuallyHidden>
-                                <label htmlFor="message">Message</label>
-                            </VisuallyHidden>
-                            <textarea
-                                variant="textarea"
-                                placeholder="Message"
-                                id="message"
-                                name="message"
-                                rows="5"
-                            />
-                        </div>
-                        <button type="submit" variant="button.primary" colStart="span 2">
-                            Send message
-            </button>
-                    </form>
+
+        <div variant="container" width="90%|70%" maxWidth="850px" py="32px|80px|96px|48px">
+            <form onSubmit={handleOnSubmit} display="grid" col="1|repeat(2, auto)" gap="4" mt="4" w="full|auto">
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="name">Name</label>
+                    </VisuallyHidden>
+                    <input
+                        variant="input"
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                    />
                 </div>
-            </div>
-        </section>
-    )
-}
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="email">Email</label>
+                    </VisuallyHidden>
+                    <input
+                        variant="input"
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                    />
+                </div>
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="phone">Phone</label>
+                    </VisuallyHidden>
+                    <input
+                        variant="input"
+                        type="tel"
+                        id="phone"
+                        name="phone"
+                        placeholder="Phone"
+                    />
+                </div>
+                <div>
+                    <VisuallyHidden>
+                        <label htmlFor="subject">Subject</label>
+                    </VisuallyHidden>
+                    <select variant="select" id="subject" name="subject" height="36px|48px" color="gray">
+                        <option value="">Subject</option>
+                        <option value="Online Order">Place an Order</option>
+                        <option value="Custom Cake">Custom Cake Design</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </div>
+                <div colStart="span 2">
+                    <VisuallyHidden>
+                        <label htmlFor="message">Message</label>
+                    </VisuallyHidden>
+                    <textarea
+                        variant="textarea"
+                        placeholder="Message"
+                        id="message"
+                        name="message"
+                        rows="5"
+                    />
+                </div>
+                <button type="submit" variant="button.primary" colStart="span 2">Send</button>
+            </form>
+        </div>
+
+    );
+};
+
+export default MyForm;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { Layout } from "../components"
 import InnerHero from "../blocks/innerhero"
+import Form from "../blocks/form"
 
 export default function ContactPage() {
     return (
@@ -11,61 +12,9 @@ export default function ContactPage() {
                 subheading="Contact Us"
                 heading="Want to place an order or inquire about a custom cake? Fill out the form below:"
             />
-            <form netlify
-                display="grid"
-                col="1|repeat(2, auto)"
-                gap="4"
-                mt="4"
-                w="full|auto"
-            >
-                <div>
-                    <input
-                        variant="input"
-                        type="text"
-                        id="name"
-                        name="name"
-                        placeholder="Name"
-                    />
-                </div>
-                <div>
-                    <input
-                        variant="input"
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                    />
-                </div>
-                <div>
-                    <input
-                        variant="input"
-                        type="tel"
-                        id="phone"
-                        name="phone"
-                        placeholder="Phone"
-                    />
-                </div>
-                <div>
-                    <select variant="select" id="subject" name="subject">
-                        <option value="">Subject</option>
-                        <option value="one">Online Order</option>
-                        <option value="two">Custom Cake</option>
-                        <option value="two">Other</option>
-                    </select>
-                </div>
-                <div colStart="span 2">
-                    <textarea
-                        variant="textarea"
-                        placeholder="Message"
-                        id="message"
-                        name="message"
-                        rows="5"
-                    />
-                </div>
-                <button type="submit" variant="button.primary" colStart="span 2">
-                    Send message
-                </button>
-            </form>
+
+            <Form />
+
         </Layout>
     )
-}
+};
