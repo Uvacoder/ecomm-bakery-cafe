1dfe49bd2108c03a67dd44b5dafc9d2f9a756383
Matt Safaii <matt@mgency.com>
2021-12-08 15:39:10 -0800

    Update footer.js
    Updated hours

--- src/components/footer.js
+++ src/components/footer.js
@@ -33,9 +33,9 @@ export function Footer({ name, copyright, links, iconLinks, children, ...props }
               <div>
                 <h3 variant="heading.h2" letterSpacing="1" fontFamily="headingLight" color="background" pb="1">Hours</h3>
                 <p fontSize="lg|xl" color="muted">
-                  Tues - Sat: 8am-7pm <br />
+                  Mon: 9am-5pm
+                  Tue-Sat: 8am-7pm <br />
                   Sun: 9am-7pm <br />
-                  Mon: Closed
                 </p>
               </div>
               <div>
